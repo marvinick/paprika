45fc98fefbf04ccfc94267fe92a247c4285d41cd
marvinick <marviluw@gmail.com>
2014-04-20 09:40:08 -0500

    started twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paprika2::Application.config.secret_key_base = 'eefbc52ea0e31863919f6fb2a32e1bb8e56183b574bc94bd76e7338ffe52d1ac6ffcd15f03c0fcea64eee687c81427d7a652b496acf74d92301ab99af4ac2831'
