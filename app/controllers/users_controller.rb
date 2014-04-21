33f46e1b6d7211da21b393ce5f7c93261efa3c06
marvinick <marviluw@gmail.com>
2014-04-21 15:12:26 -0500

    show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,26 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "Thank you for registering with us. We promise you'll have a great time"
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
