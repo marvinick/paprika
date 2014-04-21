9ddf47f4447a112b7dd87e59a4cfad825d7a7ca6
marvinick <marviluw@gmail.com>
2014-04-21 14:45:40 -0500

    form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Paprika2::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :users
+  root to: 'users#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
